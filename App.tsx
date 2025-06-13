d7ca15fea9a74af814333f38bb0d6eebeabb751d
Yosef Hayim <yosefisabag+03@gmail.com>
2025-06-13 13:16:12 +0300

    finish init project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
 import './global.css';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </>
   );
 }
