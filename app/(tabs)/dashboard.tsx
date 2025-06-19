465ca214232fdbd837b0fc0a452293f2b354101b
yosefhayim <yosefisabag+03@gmail.com>
2025-06-19 16:01:30 +0300

    working on dashboard

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -7,7 +7,6 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import Paragraph from 'components/Paragraph';
 import Title from 'components/Title';
-import { colors } from 'constants/colors';
 
 const DashboardScreen = () => {
   return (
@@ -16,44 +15,35 @@ const DashboardScreen = () => {
         <Title titleText="Hello, Jospeh" />
         <Paragraph text={'Keep up the great work'} extraStyle={{ textAlign: 'center' }} />
       </View>
-      <View style={{ width: '100%' }}>
-        <CustomButton buttonType="confirmaiton" buttonText="Start Daily Challenge" />
-      </View>
       <View
         style={{
           width: '100%',
           justifyContent: 'space-evenly',
           alignItems: 'center',
           flexDirection: 'row',
-          borderRadius: 122,
+          borderRadius: 100,
           padding: 10,
         }}>
         <View
           style={{
             alignItems: 'center',
-            backgroundColor: '#F5F5F5',
+            backgroundColor: '#FFFFFF',
             padding: 5,
             width: 60,
             borderRadius: 12,
-            shadowOpacity: 0.3,
-            shadowOffset: { width: 2, height: 2 },
-            elevation: 5,
           }}>
           <Text>XP</Text>
           <MaterialCommunityIcons name="brain" size={24} color="black" />
-          <Text>3</Text>
+          <Text>3500</Text>
         </View>
         <View
           style={{
             width: 60,
             justifyContent: 'center',
             alignItems: 'center',
-            backgroundColor: '#F5F5F5',
+            backgroundColor: '#FFFFFF',
             padding: 5,
             borderRadius: 12,
-            shadowOpacity: 0.3,
-            shadowOffset: { width: 2, height: 2 },
-            elevation: 5,
           }}>
           <Text>Streak</Text>
           <MaterialCommunityIcons name="fire-circle" size={24} color="black" />
@@ -63,32 +53,54 @@ const DashboardScreen = () => {
           style={{
             justifyContent: 'center',
             alignItems: 'center',
-            backgroundColor: '#F5F5F5',
+            backgroundColor: '#FFFFFF',
             padding: 5,
             borderRadius: 12,
-            shadowOpacity: 0.3,
-            shadowOffset: { width: 2, height: 2 },
-            elevation: 5,
           }}>
           <Text>Ranking</Text>
           <FontAwesome6 name="ranking-star" size={24} color="black" />
-          <Text>3</Text>
+          <Text>#3</Text>
         </View>
         <View
           style={{
             justifyContent: 'center',
             alignItems: 'center',
-            backgroundColor: '#F5F5F5',
+            backgroundColor: '#FFFFFF',
             padding: 5,
             width: 60,
             borderRadius: 12,
-            shadowOpacity: 0.3,
-            shadowOffset: { width: 2, height: 2 },
-            elevation: 5,
           }}>
           <Text>Total</Text>
           <MaterialIcons name="question-answer" size={24} color="black" />
           <Text>3</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          gap: 8,
+          borderColor: 'gray',
+          borderWidth: 0.2,
+          borderRadius: 20,
+          padding: 20,
+        }}>
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View>
+            <Paragraph
+              text={'Today`s Challenge'}
+              extraStyle={{ fontWeight: 'bold', textAlign: 'center' }}
+            />
+            <Paragraph
+              text={'Test your skills with this AI-generated quiz.'}
+              extraStyle={{ fontWeight: 'light', textAlign: 'center' }}
+            />
+          </View>
+        </View>
+        <View>
+          <CustomButton buttonType="confirmaiton" buttonText="Start Daily Challenge" />
         </View>
       </View>
     </CustomScreen>
