e2956a50a6e484ed792522fae55922edd1904cf5
yosefhayim <yosefisabag+03@gmail.com>
2025-06-20 19:46:08 +0300

    added debug of naive wind

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,18 @@
+import '../global.css';
+
 import { ActivityIndicator, View } from 'react-native';
 import { useEffect, useState } from 'react';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
+import { verifyInstallation } from 'nativewind';
 
 export default function Index() {
-  const router = useRouter();
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
+    verifyInstallation();
     const checkStatus = async () => {
       try {
         const onboardingDone = await AsyncStorage.getItem('hasCompletedOnboarding');
