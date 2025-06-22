4be77429f29504750ac1d63a228cedc61d568620
yosefhayim <yosefisabag+03@gmail.com>
2025-06-22 14:28:16 +0300

    contiune work on seed data

--- backend/seed.ts
+++ backend/seed.ts
@@ -1,32 +1,63 @@
 import { CONFIG } from './config'
+import axios from 'axios'
 import { createClient } from '@supabase/supabase-js'
 import { faker } from '@faker-js/faker'
 
 const supabase = createClient(CONFIG.supabaseUrl!, CONFIG.supabaseKey!)
 
-if (!CONFIG.supabaseUrl! || CONFIG.supabaseKey) console.log('Dotenv not working...')
+if (!CONFIG.supabaseUrl! && !CONFIG.supabaseKey) throw new Error('Not loaded')
 
 const createUsers = async (amount: number) => {
-  const firstName = faker.person.firstName()
-  const lastName = faker.person.lastName()
+  try {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
 
-  const userInfo = {
-    email: faker.internet.email({ firstName }),
-    password: faker.internet.password({ length: 6 }),
-    options: {
-      data: {
-        first_name: firstName,
-        phone: faker.phone.number(),
-        last_name: lastName,
-        age: faker.number.bigInt({ min: 18, max: 90 })
+    for (let i = 0; i < amount; i++) {
+      console.log('Uploading user number: ', i)
+
+      const userInfo = {
+        email: faker.internet.email({ firstName }),
+        password: faker.internet.password({ length: 6 }),
+        options: {
+          data: {
+            first_name: firstName,
+            phone: faker.phone.number(),
+            last_name: lastName,
+            age: faker.number.int({ min: 18, max: 90 })
+          }
+        }
       }
-    }
-  }
 
-  for (let i = 0; i < amount; i++) {
-    const { data, error } = await supabase.auth.signUp(userInfo)
+      const { data, error } = await supabase.auth.signUp(userInfo)
+      if (error) throw error
+
+      await uploadUserImgToBucket(
+        faker.image.personPortrait({ sex: undefined, size: 256 }),
+        data!.user!.id
+      )
+    }
+  } catch (error) {
+    console.error(`createUsers Fn: `, error)
+  }
+}
+
+const uploadUserImgToBucket = async (imageUrl: string, userId: string) => {
+  try {
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' })
+    const buffer = response.data
+
+    const { data, error } = await supabase.storage
+      .from('profile-images')
+      .upload(`public/${userId}.png`, buffer, {
+        contentType: 'image/png',
+        cacheControl: '3600',
+        upsert: false
+      })
+
     if (error) throw error
-    console.log(data?.user?.app_metadata)
+    console.log(`Image uploaded for user: ${userId}`, data)
+  } catch (error) {
+    console.error(`uploadUserImgToBucket error:`, error)
   }
 }
 
---
+++ backend/utils/delayFn.ts
@@ -0,0 +1,9 @@
+const wait = (ms: number) => {
+  const start = Date.now()
+  let now = start
+  while (now - start < ms) {
+    now = Date.now()
+  }
+}
+
+export default wait
