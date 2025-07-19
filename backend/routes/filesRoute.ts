25e846225abc5ad0a3261078010f8510ba2e1257
Yosef Hayim <yosefisabag+03@gmail.com>
2025-07-19 19:59:30 +0300

    added workflow folder for future workflows

--- backend/routes/filesRoute.ts
+++ backend/routes/filesRoute.ts
@@ -1,37 +1,34 @@
-import express from 'express';
-import multer from 'multer';
-import {
-  getImgContentByAI,
-  getPdfContentByAI,
-} from '../controllers/filesController';
+import { getImgContentByAI, getPdfContentByAI } from "../controllers/filesController";
+
+import express from "express";
+import multer from "multer";
 
 const router = express.Router();
 
 const uploadPdfs = multer({
-  dest: './files/pdfs/',
+  dest: "../files/pdfs/",
   fileFilter: (req, file, cb) => {
-    if (file.mimetype !== 'application/pdf') cb(null, false);
+    if (file.mimetype !== "application/pdf") cb(null, false);
   },
 });
 
 const uploadImgs = multer({
-  dest: './files/images/',
+  dest: "../files/images/",
   fileFilter: (req, file, cb) => {
-    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png')
-      cb(null, false);
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") cb(null, false);
   },
 });
 
 const uploadTxt = multer({
-  dest: './files/txts/',
+  dest: "../files/txts/",
   fileFilter: (req, file, cb) => {
-    if (file.mimetype !== 'text/plain') cb(null, false);
+    if (file.mimetype !== "text/plain") cb(null, false);
   },
 });
 
-router.post('/pdf', uploadPdfs.single('pdf'), getPdfContentByAI);
+router.post("/pdf", uploadPdfs.single("pdf"), getPdfContentByAI);
 
-router.post('/img', uploadImgs.single('img'), getImgContentByAI);
+router.post("/img", uploadImgs.single("img"), getImgContentByAI);
 
-router.post('/txt', uploadTxt.single('txt'), getImgContentByAI);
+router.post("/txt", uploadTxt.single("txt"), getImgContentByAI);
 export default router;
